cb5fca52fc80c1d58a096ea6514395b9265225e8
zack0307 <box930307@gmail.com>
2025-07-29 22:24:38 +0800

    debug virus-detect function  and malware_file_execute file

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -13,91 +13,6 @@ import torch.nn as nn
 
 device =  torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class VGG(nn.Module):
-    def __init__(self, num_classes=2):
-        super(VGG, self).__init__()
-        self.features = nn.Sequential(
-
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
-            # nn.Dropout(0.2),
-
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding = 0),
-            # nn.Dropout(0.2),
-
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding = 0),
-            # nn.Dropout(0.2),
-
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding = 0),
-            # nn.Dropout(0.2),
-
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=1, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding = 0),
-            # nn.Dropout(0.2),
-
-            nn.Conv2d(512, 1024, kernel_size=3, padding=1),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(1024, 1024, kernel_size=3, padding=1),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(1024, 1024, kernel_size=1, padding=1),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding = 0),
-            # nn.Dropout(0.2),
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
-        # num_features = 512 * (256 // 32) * (256 // 32)
-        self.classifier = nn.Sequential(
-            nn.Linear(1024 * 7 * 7, 1024),  # 壓平後的輸入尺寸為 512x7x7
-            nn.ReLU(inplace=True),
-            # nn.Dropout(p=0.5),  # 避免過擬合
-            nn.Linear(1024, num_classes),  # 輸出類別數,
-            nn.Softmax(dim = 1),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
-
 model = VGG(num_classes = 2)
 model.load_state_dict(torch.load('C:/Users/digital/Desktop/malware/malware_analysis1.pt', map_location = device))
 model.eval()
@@ -113,8 +28,11 @@ def virus_upload():
 
 @blueprint.route('/virus-detect', methods=['POST'])
 def virus_detect():
+    #讀取檔案
     detect_file = read_file()
+    #轉換markov
     markov_file = file_to_hex(detect_file)
+    #markov轉換成npy
     npy_file = markov_to_npy(markov_file)
 
     pass
